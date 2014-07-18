f33506050d73db178b61d6157050617e8ac2970a
pteromys <pteromys@gmail.com>
2014-07-18 08:59:10 -0500

    3D: Support zooming, translating, recentering.
    * Distant field is somewhat flickery, so add support for fog (uFog).
    * Widen field of view to reduce importance of distant field.
    * Precompute all properties of Vertex. (It's simpler.)
    * Recentering requires a directions theory like in the 2D version.
    * QuasiLattice3.addVerts() is now always symmetric since it's faster.
    
    Observations:
    * Initial generation is now taking more time.
    * Coordination of all 3 was a bit tricky to get right.

--- frag.c
+++ frag.c
@@ -1,10 +1,11 @@
-varying lowp vec4 vColor;
+varying lowp vec3 vColor;
+varying mediump float vFog;
 const lowp vec2 cMid = vec2(0.5);
 
 void main(void) {
 	lowp vec2 xy = gl_PointCoord - cMid;
-	lowp float d = 5.0 * dot(xy, xy);
-	lowp float w = clamp(1.0 - 4.0 * d, 0.0, 1.0);
-	gl_FragColor = clamp(vec4(clamp(vec3(1.0) - abs(2.0 * vColor.xyz), 0.0, 1.0) + vec3(w), 1.25 - d), 0.0, 1.0);
+	lowp float d = 4.0 * dot(xy, xy);
+	lowp float w = clamp(1.0 - 9.0 * d, 0.0, 1.0);
+	gl_FragColor = clamp(vec4(vColor + vec3(w), (1.0 - d)*1.125 * vFog), 0.0, 1.0);
 }
 
--- vert.c
+++ vert.c
@@ -1,14 +1,22 @@
 attribute vec4 aVPosition;
 attribute vec4 aVColor;
 
-uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
+uniform mediump vec3 uOrigin;
+uniform mediump vec3 uOriginColor;
 uniform mediump vec2 uScale;
+uniform mediump vec2 uFog;
 
-varying mediump vec4 vColor;
+varying mediump vec3 vColor;
+varying mediump float vFog;
 
 void main(void) {
-	gl_Position = uPMatrix * uMVMatrix * vec4(aVPosition.xyz * uScale.x, 1.0);
-	vColor = aVColor * uScale.y;
-	gl_PointSize = 200.0 * exp(-0.25 * dot(vColor.xyz, vColor.xyz) / 0.25) / gl_Position.w;
+	// Transform
+	gl_Position = uPMatrix * vec4((aVPosition.xyz + uOrigin) * uScale.x, 1.0);
+	vColor = (aVColor.xyz + uOriginColor) * uScale.y;
+	// Set paint parameters
+	gl_PointSize = 80.0 * exp(-0.25 * dot(vColor, vColor) / 0.4) / gl_Position.w;
+	vFog = clamp(uFog.x/gl_Position.w - uFog.y, 0.0, 1.0);
+	vColor = clamp(vec3(1.0) - abs(1.2 * vColor), 0.0, 1.0);
+	vColor /= max(vColor.x, max(vColor.y, vColor.z));
 }
