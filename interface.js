98ad89d120a59ed0514c86ff8f9a5f7abab64134
pteromys <pteromys@gmail.com>
2014-08-14 15:58:20 -0500

    Interface: Add message box.
    MessageBox mostly cribbed from hyperbolic plane project.
    
    First use: display a progress bar for point search,
    along with messages from the search worker.

--- interface.js
+++ interface.js
@@ -158,3 +158,69 @@ $(document).ready(ButtonSystem.activateRootButtons);
 
 return ButtonSystem;
 })();
+
+
+
+var MessageBox = (function () {
+
+var MessageBox = function (selector, debug) {
+	this.selector = selector;
+	this.panel = $(this.selector).find('.panel')[0];
+	this.debug = debug;
+	if (!this.panel) { this.panel = $('<div class="box">').appendTo(selector)[0]; }
+	var this_box = this;
+	$(this.panel).on('click', function (e) {
+		var t = $(e.target);
+		if (t.is('.message')) {
+			if (t.hasClass('pinned')) {
+				t.removeClass('pinned active');
+				this_box.hide(t);
+			} else if (t.hasClass('active')) {
+				t.addClass('pinned');
+			}
+		}
+	});
+};
+
+var ProgressBar = function (parent_box, text) {
+	this.parent_box = parent_box;
+	this.msgdiv = parent_box.show(text, 'progress', 86400000);
+	this.msgdiv.append($('<div class="progressbar"><div></div></div>'));
+};
+
+ProgressBar.prototype = {
+	set: function (amount) {
+		var percent = (amount * 100) + '%';
+		this.msgdiv.find('.progressbar > div').css({width: percent});
+		if (amount >= 1) {
+			window.setTimeout(this.parent_box.hide.bind(this.parent_box, this.msgdiv), 100);
+		}
+	},
+};
+
+MessageBox.prototype = {
+	del: function (msg) {
+		if (msg && msg.is && msg.is('.message') && !msg.is('.pinned')) {
+			msg.remove();
+		}
+	},
+	hide: function (msg) {
+		if (msg && msg.is && msg.is('.message') && !msg.is('.pinned')) {
+			msg.removeClass('active');
+			window.setTimeout(this.del.bind(this, msg), 150);
+		}
+	},
+	show: function (text, cls, expiry) {
+		var msg = $('<div class="message active">').text(text).addClass(cls);
+		if (msg.hasClass('debug') && !this.debug) { return $(); }
+		$(this.panel).append(msg);
+		window.setTimeout(this.hide.bind(this, msg), expiry || 5000);
+		return msg;
+	},
+	makeProgress: function (text) {
+		return new ProgressBar(this, text);
+	},
+};
+
+return MessageBox;
+})();
